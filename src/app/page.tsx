1ece5089cc6b7730bb64ecaa90992958eecb3a2f
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-21 21:36:40 +0330

    chore (tailwindcss) : add prettier plugin tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main className="flex min-h-screen p-24">Resume Ali Seyedi</main>;
+  return (
+    <main className="flex min-h-screen flex-col bg-red-500 p-24 text-white">
+      <p className="font-serif">Resume Ali Seyedi</p>
+    </main>
+  );
 }
