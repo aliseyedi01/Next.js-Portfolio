a1db27aee9086b3066b1605176a7bbc146df1c17
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-22 20:08:08 +0330

    feat(shadcn): add shadcn and shadcn button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/ui/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-red-500 p-24 text-white">
+    <main className="flex gap-2 p-24">
       <p className="font-serif">Resume Ali Seyedi</p>
+      <Button size="sm" variant="destructive">
+        Home
+      </Button>
     </main>
   );
 }
