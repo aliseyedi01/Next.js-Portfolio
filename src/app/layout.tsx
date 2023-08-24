f8c5c78fd43c74513cf944b9323f6784e4be897e
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-24 18:50:44 +0330

    feat ( layout ) : add scroll-smooth to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ scrollBehavior: "smooth" }}>
       <body className={`${inter.className} `}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
