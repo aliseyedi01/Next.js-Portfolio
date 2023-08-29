6a8851ef0232cf28f02b7c8a09bc2b1f4a038aa5
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-29 23:49:12 +0330

    feat(Navbar): Add scroll-based effect for navigation items based on div IDs

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 // react
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   HomeIcon,
   InfoCircledIcon,
@@ -13,6 +13,7 @@ import {
 import Link from "next/link";
 // component
 import { ModeToggle } from "../theme/ModeToggle";
+import { Button } from "../ui/button";
 // types
 interface NavItem {
   name: string;
@@ -23,69 +24,105 @@ interface NavItem {
 const navItemsData: NavItem[] = [
   {
     name: "Home",
-    link: "/#home",
+    link: "#home",
     icon: <HomeIcon />,
   },
   {
     name: "About",
-    link: "/#about",
+    link: "#about",
     icon: <InfoCircledIcon />,
   },
   {
     name: "Skills",
-    link: "/#skills",
+    link: "#skills",
     icon: <RocketIcon />,
   },
   {
     name: "Projects",
-    link: "/#projects",
+    link: "#projects",
     icon: <LaptopIcon />,
   },
   {
     name: "Contact",
-    link: "/#contact",
+    link: "#contact",
     icon: <ChatBubbleIcon />,
-  },
-  {
-    name: "Resume",
-    link: "/resume",
-    icon: <FileTextIcon />,
   },
 ];
 
-export const Navbar: React.FC = () => (
-  <div className="relative">
-    {/* Desktop & Mobile header */}
-    <header className="fixed top-0 flex h-12  w-full items-center justify-between p-2 px-4 backdrop-blur-md">
-      {/* Title */}
-      <h2 className="text-sm md:text-base">Ali Seyedi</h2>
-      <div className="flex items-center gap-12">
+export const Navbar: React.FC = () => {
+  const [activeSection, setActiveSection] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Callback function when an observed section comes into view
+    const handleSectionIntersect = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    };
+    // Create an Intersection Observer instance
+    const observer = new IntersectionObserver(handleSectionIntersect, {
+      rootMargin: "-50% 0px -50% 0px",
+    });
+    // Observe each section
+    navItemsData.forEach((item) => {
+      const section = document.querySelector(item.link);
+      if (section) {
+        observer.observe(section);
+      }
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="relative">
+      {/* Desktop & Mobile header */}
+      <header className="fixed top-0 flex h-12  w-full items-center justify-between p-2 px-4 backdrop-blur-md">
+        {/* Title */}
+        <h2 className="text-sm md:text-base">Ali Seyedi</h2>
         {/* Items */}
-        <nav className="hidden items-center gap-5 md:flex">
+        <nav className="hidden translate-x-6 items-center gap-6 md:flex">
           {navItemsData.map((item) => (
             <Link
               href={item.link}
               key={item.name}
-              className="flex items-center gap-1 hover:border-b hover:border-red-400"
+              className={`flex items-center gap-1 hover:border-b ${
+                activeSection == item.name.toLowerCase()
+                  ? "border-b border-indigo-500 text-indigo-800"
+                  : ""
+              }`}
             >
               {item.name}
             </Link>
           ))}
         </nav>
         {/* Change Mode */}
-        <ModeToggle />
-      </div>
-    </header>
-
-    {/* Mobile header */}
-    <header className="fixed bottom-2 left-0 w-full p-2 md:hidden">
-      <nav className="mx-5 flex items-center justify-center gap-8 rounded-full border border-indigo-400 py-2 backdrop-blur-lg dark:border-red-500">
-        {navItemsData.map((item) => (
-          <Link key={item.link} href={item.link} className="flex items-center gap-1">
-            <span className="font-bold text-indigo-600 dark:text-yellow-400">{item.icon}</span>
+        <div className="flex items-center gap-4">
+          <Link href="/resume">
+            <Button size="sm">Resume</Button>
           </Link>
-        ))}
-      </nav>
-    </header>
-  </div>
-);
+          <ModeToggle />
+        </div>
+      </header>
+      {/* Mobile header */}
+      <header className="fixed bottom-2 left-0 w-full p-2 md:hidden">
+        <nav className="mx-5 flex items-center justify-center gap-8 rounded-full border border-indigo-400 py-2 backdrop-blur-lg dark:border-red-500">
+          {navItemsData.map((item) => (
+            <Link key={item.link} href={item.link} className="flex items-center gap-1">
+              <span
+                className={`font-bold text-indigo-600 transition-all duration-100 dark:text-yellow-400 ${
+                  activeSection == item.name.toLowerCase() ? "scale-125 text-red-600 shadow-lg" : ""
+                }`}
+              >
+                {item.icon}
+              </span>
+            </Link>
+          ))}
+        </nav>
+      </header>
+    </div>
+  );
+};
