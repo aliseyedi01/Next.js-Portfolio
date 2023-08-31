34f1271cb0b8d1005b27f81005b731f7f67218f6
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-08-31 22:57:08 +0330

    style (modeToggle) : Improve icon set and style design

--- src/components/theme/ModeToggle.tsx
+++ src/components/theme/ModeToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { BsSunFill, BsMoonStarsFill, BsDisplayFill } from "react-icons/bs";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -18,16 +18,28 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon" className="relative">
+          <BsSunFill className="rotate-0 scale-100 text-xl transition-all dark:-rotate-90 dark:scale-0" />
+          <BsMoonStarsFill className="absolute rotate-90 scale-0 text-xl transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>System</DropdownMenuItem>
+      <DropdownMenuContent
+        align="end"
+        className=" border-indigo-400 bg-background font-ubuntu dark:border dark:border-indigo-600"
+      >
+        <DropdownMenuItem onClick={() => setTheme("light")} className="cursor-pointer">
+          <BsSunFill className="mr-2" />
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")} className="cursor-pointer">
+          <BsMoonStarsFill className="mr-2" />
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")} className="cursor-pointer">
+          <BsDisplayFill className="mr-2" />
+          System
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
