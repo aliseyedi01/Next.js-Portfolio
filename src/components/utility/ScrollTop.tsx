715f7e0f266fa4dbde55c29541fda89891d1a895
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2023-09-07 18:19:09 +0330

    feat(ScrollTop): Add progress circle for scroll percentage indicator

--- src/components/utility/ScrollTop.tsx
+++ src/components/utility/ScrollTop.tsx
@@ -2,6 +2,7 @@
 import { useScrollPercentage } from "@/hooks/useScrollPercentage";
 // react
 import React, { useState, useEffect } from "react";
+// import CircleProgressBar from "./CircleProgressBar";
 
 const ScrollTop: React.FC = () => {
   const scrollPercentage = useScrollPercentage();
@@ -11,29 +12,56 @@ const ScrollTop: React.FC = () => {
     setShowTopButton(Number(scrollPercentage) < 80);
   }, [scrollPercentage]);
 
-  const handleClick = () => {
+  const handleTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  let widthCircle = 60;
+  let percentage = 100 - scrollPercentage;
+  let radius = widthCircle / 2 - 10;
+  let dashArray = 2 * Math.PI * radius;
+  let dashOffset = dashArray - (dashArray * percentage) / 100;
+
   return (
-    <div
-      className={`fixed bottom-4 right-4 h-12 w-12  cursor-pointer overflow-hidden rounded-full border border-blue-600 bg-blue-600 ${
-        showTopButton ? "hidden md:block" : "hidden"
-      } `}
-    >
-      <div className="relative h-full w-full">
-        {/* background scroll */}
-        <div
-          onClick={handleClick}
-          style={{ height: `${scrollPercentage}%` }}
-          className="absolute inset-0 bg-background text-3xl"
-        >
-          {/* svg top */}
-          <div className="svg-top absolute left-[7px] top-[5.5px] h-8 w-8 bg-indigo-950" />
-        </div>
-      </div>
+    <div className={`fixed bottom-4 right-4 z-50 ${showTopButton ? "hidden md:block" : "hidden"}`}>
+      {/* circle progress bar  */}
+      <svg width={widthCircle} height={widthCircle} viewBox={`0 0 ${widthCircle} ${widthCircle}`}>
+        <circle
+          cx={widthCircle / 2}
+          cy={widthCircle / 2}
+          strokeWidth="8px"
+          r={radius}
+          className="fill-none stroke-indigo-400 dark:stroke-indigo-400"
+        />
+        <circle
+          cx={widthCircle / 2}
+          cy={widthCircle / 2}
+          strokeWidth="8px"
+          r={radius}
+          className="circle-2 fill-none stroke-indigo-600 dark:stroke-indigo-600"
+          transform={`rotate(-90 ${widthCircle / 2} ${widthCircle / 2})`}
+          style={{
+            strokeDasharray: dashArray,
+            strokeDashoffset: dashOffset,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+          }}
+        />
+      </svg>
+      {/* Icon Top */}
+      <div
+        className="svg-top absolute left-[16px] top-[13px] h-7 w-7 cursor-pointer bg-indigo-800 dark:bg-indigo-400"
+        onClick={handleTop}
+      />
     </div>
   );
 };
 
 export default ScrollTop;
+
+{
+  /* svg to top */
+}
+{
+  /* <div className="svg-top absolute left-[7px] top-[5.5px] h-8 w-8 bg-indigo-950" /> */
+}
